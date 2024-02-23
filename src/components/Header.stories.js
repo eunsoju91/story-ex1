b7fe68f60f5b380cae9cd5baefe14e0a07056b1e
eunsoju91 <eunsoju91@gmail.com>
2024-02-23 11:28:42 +0900

    자동 배포 테스트

---
+++ src/components/Header.stories.js
@@ -0,0 +1,19 @@
+import Header from "./Header";
+
+export default {
+  component: Header,
+  title: "Header",
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  args: {
+    task: {
+      id: "1",
+      title: "header",
+      state: "header",
+    },
+  },
+};
+
+
