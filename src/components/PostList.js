266ba9309a23946005cb7e4d8fe99672b9a1117f
eunsoju91 <eunsoju91@gmail.com>
2024-03-15 14:36:31 +0900

    수정!!!!

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,25 @@
-
-import React from 'react';
+import React, { useEffect } from 'react';
 import Post from './Post';
+
 
 export default function PostList({post}) {
   const postInOrder = [
     ...post
   ];
+
+  useEffect(() => {
+    var postList = document.querySelector('.post-list');
+    var postItems = document.querySelectorAll('.post-list > .post-item');
+
+    if (postList) {
+      postList.classList.add('on');
+    }
+
+    postItems.forEach(function(postItem, i) {
+      postItem.style.setProperty("--char-index", i);
+    });
+  }, []);
+
   return (
     <ul id="columns" className="post-list">
       {postInOrder.map((post) => (
